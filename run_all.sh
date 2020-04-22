579f4e043fbac4b6e1724f5fe798cd2efaea5c9c
Tao Hu <taohu620@gmail.com>
2020-04-22 20:06:05 +0200

    u

--- run_all.sh
+++ run_all.sh
@@ -13,3 +13,5 @@ python main.py --gpu 2 --batch_size 4 --test_batch_size 4 --method vasa --novel_
 python main.py --gpu 0 --method baseline --novel_num 5 --log_action d &&
 python main.py --gpu 1 --method va --novel_num 5 --log_action d &&
 python main.py --gpu 2  --method vasa --novel_num 5 --log_action d
+
+
