488789f806726e4bc0a604da79a2bf307d17605f
Tao Hu <taohu620@gmail.com>
2020-04-24 22:56:21 +0200

    u

--- data_generate/__init__.py
+++ data_generate/__init__.py
@@ -1,6 +1,2 @@
 from . import activitynet_label
-from . import activitynet_label_100_20_80
-from . import activitynet_label_80_20_100
-from . import activitynet_label_120_20_60
-from . import activitynet_label_40_20_140
 from . import activitynet_label_60_20_60_unseen60
